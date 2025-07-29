14de7f7e659ae8558a7b1b9cb888d6044e57fe74
boubaker1key <125211353+boubaker1key@users.noreply.github.com>
2025-07-29 01:37:20 +0100

    auth.ts

--- app/auth.ts
+++ app/auth.ts
@@ -17,10 +17,9 @@ export const {
       async authorize({ email, password }: any) {
       //  try{
         let user = await getUser(email);
-        if (!user || user.length === 0) return false;//{ error: 'Invalid User!' };
+        if (user.length === 0) return null;
         let passwordsMatch = await compare(password, user[0].password!);
-        if (!passwordsMatch) return false;//{ error: 'Invalid Password!' };
-        return user[0] as any;
+        if (passwordsMatch) return user[0] as any;
         //}catch(err){
        //   return { error: err instanceof Error ? err.message : 'Error credentials!' }; 
         //}
